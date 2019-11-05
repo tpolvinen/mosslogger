e3591f9e80e3a3a4ecf5aeb70bea272945ba3b58
Tatu P <tatu.polvinen@helsinki.fi>
2019-11-05 18:19:37 +0200

    Testing readings with 5V pin voltage, leaving LM4040 out due to a voltage drop

--- mosslogger.ino
+++ mosslogger.ino
@@ -1,12 +1,8 @@
-
-// Trying readings with LS4040 reference voltage of 4.096V and use this gain setting:
-// ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
-
+// Getting readings with voltage from MEGA 5V pin
 
 #include <Controllino.h>
 #include <Wire.h>
 #include <Adafruit_ADS1015.h>
-
 
 Adafruit_ADS1115 ads0(0x48);
 Adafruit_ADS1115 ads1(0x49);
@@ -15,63 +11,51 @@ Adafruit_ADS1115 ads3(0x4B);
 
 unsigned long previousMillis = 0;
 unsigned long missedMillis = 0;
+//unsigned long measurementMillis = 0;
 int16_t counter = 0;
 
-long oneSecondAverage00 = 0;
-long oneSecondAverage01 = 0;
-long oneSecondAverage02 = 0;
-long oneSecondAverage03 = 0;
+long oneSecondAverage00 = 0, oneSecondAverage01 = 0, oneSecondAverage02 = 0, oneSecondAverage03 = 0;
+long oneSecondAverage10 = 0, oneSecondAverage11 = 0, oneSecondAverage12 = 0, oneSecondAverage13 = 0;
+long oneSecondAverage20 = 0, oneSecondAverage21 = 0, oneSecondAverage22 = 0, oneSecondAverage23 = 0;
+long oneSecondAverage30 = 0, oneSecondAverage31 = 0, oneSecondAverage32 = 0, oneSecondAverage33 = 0;
 
-long oneSecondAverage10 = 0;
-long oneSecondAverage11 = 0;
-long oneSecondAverage12 = 0;
-long oneSecondAverage13 = 0;
+int16_t measurement00, measurement01, measurement02, measurement03;
+int16_t measurement10, measurement11, measurement12, measurement13;
+int16_t measurement20, measurement21, measurement22, measurement23;
+int16_t measurement30, measurement31, measurement32, measurement33;
 
-long oneSecondAverage20 = 0;
-long oneSecondAverage21 = 0;
-long oneSecondAverage22 = 0;
-long oneSecondAverage23 = 0;
+void setup()
 
-long oneSecondAverage30 = 0;
-long oneSecondAverage31 = 0;
-long oneSecondAverage32 = 0;
-long oneSecondAverage33 = 0;
+Serial.begin(9600);
 
-int16_t measurement00;
-int16_t measurement01;
-int16_t measurement02;
-int16_t measurement03;
+// The ADC input range (or gain) can be changed via the following
+// functions, but be careful never to exceed VDD +0.3V max, or to
+// exceed the upper and lower limits if you adjust the input range!
+// Setting these values incorrectly may destroy your ADC!
+//                                                                ADS1115
+//                                                                -------
+// ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 0.1875mV (default)
+// ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
+// ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 0.0625mV
+// ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.03125mV
+// ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.015625mV
+// ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.0078125mV
 
-int16_t measurement10;
-int16_t measurement11;
-int16_t measurement12;
-int16_t measurement13;
+ads0.setGain(GAIN_TWOTHIRDS);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
+ads1.setGain(GAIN_TWOTHIRDS);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
+ads2.setGain(GAIN_TWOTHIRDS);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
+ads3.setGain(GAIN_TWOTHIRDS);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
 
-int16_t measurement20;
-int16_t measurement21;
-int16_t measurement22;
-int16_t measurement23;
-
-int16_t measurement30;
-int16_t measurement31;
-int16_t measurement32;
-int16_t measurement33;
-
-void setup() {
-  Serial.begin(9600);
-  ads0.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
-  ads1.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
-  ads2.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
-  ads3.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 0.125mV
-  ads0.begin();
-  ads1.begin();
-  ads2.begin();
-  ads3.begin();
+ads0.begin();
+ads1.begin();
+ads2.begin();
+ads3.begin();
 }
 
 void loop() {
 
   previousMillis = millis();
+  //  measurementMillis = millis();
 
   while (millis() - previousMillis <= 1000) {
     measurement00 = ads0.readADC_SingleEnded(0);
@@ -117,11 +101,10 @@ void loop() {
     oneSecondAverage33 += measurement33;
   }
 
-  missedMillis = millis();
+  //missedMillis = millis();
 
-  Serial.print(counter);
-  Serial.print(" rounds of readings in 1 sec, total readings: ");
-  Serial.println(counter * 16);
+  //  Serial.print(counter);
+  //  Serial.print(" rounds of readings in 1 sec, total readings: ");
 
   oneSecondAverage00 /= counter;
   oneSecondAverage01 /= counter;
@@ -143,45 +126,48 @@ void loop() {
   oneSecondAverage32 /= counter;
   oneSecondAverage33 /= counter;
 
-  Serial.print("One second averages:  ");
   Serial.print(oneSecondAverage00);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage01);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage02);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage03);
-  Serial.print("  ");
+  Serial.print(",");
 
   Serial.print(oneSecondAverage10);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage11);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage12);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage13);
-  Serial.print("  ");
+  Serial.print(",");
+
+  Serial.print(",");
 
   Serial.print(oneSecondAverage20);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage21);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage22);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage23);
-  Serial.print("  ");
+  Serial.print(",");
 
   Serial.print(oneSecondAverage30);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage31);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage32);
-  Serial.print("  ");
+  Serial.print(",");
   Serial.print(oneSecondAverage33);
-  Serial.println("  ");
 
+  //  Serial.print(",");
+  //  measurementMillis = millis() - measurementMillis;
+  //  Serial.print(measurementMillis);
 
-
+  Serial.println();
 
   counter = 0;
 
@@ -205,12 +191,12 @@ void loop() {
   oneSecondAverage32 = 0;
   oneSecondAverage33 = 0;
 
-  missedMillis = millis() - missedMillis;
+  //missedMillis = millis() - missedMillis;
 
-  Serial.print("Missed milliseconds while printing and counting: ");
-  Serial.println(missedMillis);
+  //  Serial.print("Missed milliseconds while printing and counting: ");
+  //  Serial.println(missedMillis);
 
-  missedMillis = 0;
+  //missedMillis = 0;
 
   // in one second getting 7 rounds of readings, 128 milliseconds missed in printing and counting
 
@@ -218,61 +204,5 @@ void loop() {
 
   // for 8 single-ended readings * 100, time is 7115 milliseconds
   // for 16 single-ended readings * 100, time is 14230 milliseconds
-
-
-
-
-  //  Serial.print("ads0:   ");
-  //  Serial.print(measurement00);
-  //  Serial.print("  ");
-  //  Serial.print(measurement01);
-  //  Serial.print("  ");
-  //  Serial.print(measurement02);
-  //  Serial.print("  ");
-  //  Serial.println(measurement03);
-
-  //  Serial.print("  Voltage 0: ");
-  //  Voltage = (measurement00 * 0.125) / 1000;
-  //  Serial.print(Voltage, 4);
-  //  Serial.print("  ");
-  //  Voltage = (measurement01 * 0.125) / 1000;
-  //  Serial.println(Voltage, 4);
-
-  //  Serial.print("ads1:   ");
-  //  Serial.print(measurement10);
-  //  Serial.print("  ");
-  //  Serial.print(measurement11);
-  //  Serial.print("  ");
-  //  Serial.print(measurement12);
-  //  Serial.print("  ");
-  //  Serial.println(measurement13);
-
-  //  Serial.print("  Voltage 1: ");
-  //  Voltage = (measurement10 * 0.125) / 1000;
-  //  Serial.print(Voltage, 4);
-  //  Serial.print("  ");
-  //  Voltage = (measurement11 * 0.125) / 1000;
-  //  Serial.println(Voltage, 4);
-
-  //  Serial.print("ads2:   ");
-  //  Serial.print(measurement20);
-  //  Serial.print("  ");
-  //  Serial.print(measurement21);
-  //  Serial.print("  ");
-  //  Serial.print(measurement22);
-  //  Serial.print("  ");
-  //  Serial.println(measurement23);
-
-  //  Serial.print("ads3:   ");
-  //  Serial.print(measurement30);
-  //  Serial.print("  ");
-  //  Serial.print(measurement31);
-  //  Serial.print("  ");
-  //  Serial.print(measurement32);
-  //  Serial.print("  ");
-  //  Serial.println(measurement33);
-
-  //  delay(500);
-
 
 }
